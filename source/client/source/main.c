49866fb6d3c8131769918c9086d0668feb6559c1
onisim <onisim.balan99@e-uvt.ro>
2024-04-13 14:56:07 +0300

    Basic connection

--- source/client/source/main.c
+++ source/client/source/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int main(int argc, char** argv)
-{
-    puts("client");
-    return 0;
+#include "config.h"
+
+int main(int argc, char **argv) {
+  init_config(argc, argv);
+
+  puts("client");
+  return 0;
 }
--- source/server/source/main.c
+++ source/server/source/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+#include "config.h"
 #include "server.h"
 
-int main(int argc, char** argv)
-{
-    puts("server");
-    return 0;
+int main(int argc, char **argv) {
+  init_config(argc, argv);
+  init_server();
+
+  puts("server");
+  return 0;
 }
